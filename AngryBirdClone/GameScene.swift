379c7d5632211313380c0f94a0a6d911ca4f49b5
Mahmut Ersoy <mahmutersooy@gmail.com>
2023-10-01 21:03:10 +0300

    Added scoring logic for bird collisions with boxes

--- AngryBirdClone/GameScene.swift
+++ AngryBirdClone/GameScene.swift
@@ -8,7 +8,7 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
  //   var bird2 = SKSpriteNode()
     
@@ -24,6 +24,14 @@ class GameScene: SKScene {
     
     var originalPosition : CGPoint?
     
+    enum ColliderType : UInt32 {
+        case Bird = 1
+        case Box = 2
+    }
+    
+    var score = 0
+    var scoreLabel = SKLabelNode()
+    
     override func didMove(to view: SKView) {
       /*
         let texture = SKTexture(imageNamed: "bird")
@@ -36,6 +44,7 @@ class GameScene: SKScene {
         
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         self.scene?.scaleMode = .aspectFit
+        self.physicsWorld.contactDelegate = self
         
         // bird
         bird = childNode(withName: "bird") as! SKSpriteNode
@@ -47,6 +56,10 @@ class GameScene: SKScene {
         bird.physicsBody?.isDynamic = true
         bird.physicsBody?.mass = 0.15
         originalPosition = bird.position
+        
+        bird.physicsBody?.contactTestBitMask = ColliderType.Bird.rawValue
+        bird.physicsBody?.categoryBitMask = ColliderType.Bird.rawValue
+        bird.physicsBody?.collisionBitMask = ColliderType.Box.rawValue
         
         // box
         
@@ -60,12 +73,16 @@ class GameScene: SKScene {
         box1.physicsBody?.allowsRotation = true
         box1.physicsBody?.mass = 0.4
         
+        box1.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
         box2 = childNode(withName: "box2") as! SKSpriteNode
         box2.physicsBody = SKPhysicsBody(rectangleOf: size)
         box2.physicsBody?.affectedByGravity = true
         box2.physicsBody?.isDynamic = true
         box2.physicsBody?.allowsRotation = true
         box2.physicsBody?.mass = 0.4
+        
+        box2.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
         
         box3 = childNode(withName: "box3") as! SKSpriteNode
         box3.physicsBody = SKPhysicsBody(rectangleOf: size)
@@ -74,6 +91,8 @@ class GameScene: SKScene {
         box3.physicsBody?.allowsRotation = true
         box3.physicsBody?.mass = 0.4
         
+        box3.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
         box4 = childNode(withName: "box4") as! SKSpriteNode
         box4.physicsBody = SKPhysicsBody(rectangleOf: size)
         box4.physicsBody?.affectedByGravity = true
@@ -81,14 +100,35 @@ class GameScene: SKScene {
         box4.physicsBody?.allowsRotation = true
         box4.physicsBody?.mass = 0.4
         
+        box4.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
         box5 = childNode(withName: "box5") as! SKSpriteNode
         box5.physicsBody = SKPhysicsBody(rectangleOf: size)
         box5.physicsBody?.affectedByGravity = true
         box5.physicsBody?.isDynamic = true
         box5.physicsBody?.allowsRotation = true
         box5.physicsBody?.mass = 0.4
+        
+        box5.physicsBody?.collisionBitMask = ColliderType.Bird.rawValue
+        
+        // label
+        
+        scoreLabel.fontName = "Helvetica"
+        scoreLabel.fontSize = 60
+        scoreLabel.text = "0"
+        scoreLabel.position = CGPoint(x: 0, y: self.frame.height / 4)
+        scoreLabel.zPosition = 2
+        self.addChild(scoreLabel)
     }
     
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        if contact.bodyA.collisionBitMask == ColliderType.Bird.rawValue || contact.bodyB.collisionBitMask == ColliderType.Bird.rawValue {
+            
+            score += 1
+            scoreLabel.text = String(score)
+        }
+    }
     
     func touchDown(atPoint pos : CGPoint) {
         
@@ -202,6 +242,11 @@ class GameScene: SKScene {
                 bird.physicsBody?.angularVelocity = 0
                 bird.zPosition = 1
                 bird.position = originalPosition!
+                
+                
+                score = 0
+                scoreLabel.text = String(score)
+                
                 gameStarted = false
             }
         }
